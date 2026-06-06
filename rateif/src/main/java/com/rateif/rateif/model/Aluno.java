f14be6a35dfe51deed2157620126399063015802
Matheus-LucianoC <matheus2909.meg45@gmail.com>
2026-06-05 22:29:13 -0300

    Primeira versão do projeto

---
+++ rateif/src/main/java/com/rateif/rateif/model/Aluno.java
@@ -0,0 +1,98 @@
+package com.rateif.rateif.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "aluno")
+public class Aluno {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_aluno")
+    private Integer id;
+
+    @Column(name = "matricula")
+    private String matricula;
+
+    private Integer idade;
+
+    @Column(name = "email_institucional")
+    private String emailInstitucional;
+
+    private Boolean status;
+
+    @OneToOne
+    @JoinColumn(name = "id_usuario")
+    private Usuario usuario;
+
+    @ManyToOne
+    @JoinColumn(name = "id_turma")
+    private Turma turma;
+
+    public Aluno() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public Integer getIdade() {
+        return idade;
+    }
+
+    public void setIdade(Integer idade) {
+        this.idade = idade;
+    }
+
+    public String getEmailInstitucional() {
+        return emailInstitucional;
+    }
+
+    public void setEmailInstitucional(String emailInstitucional) {
+        this.emailInstitucional = emailInstitucional;
+    }
+
+    public Boolean getStatus() {
+        return status;
+    }
+
+    public void setStatus(Boolean status) {
+        this.status = status;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public Turma getTurma() {
+        return turma;
+    }
+
+    public void setTurma(Turma turma) {
+        this.turma = turma;
+    }
+}
---
+++ rateif/src/test/java/com/rateif/rateif/RateifApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rateif.rateif;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RateifApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
